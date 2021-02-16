b587dd055fd2361eede8092c934b26dad2e970f4
Yudai-Saito <yudai.saito.dev@gmail.com>
2021-02-16 16:14:32 +0900

    Add: ファイル出力

--- logging/logging_practice/logging_lesson.py
+++ logging/logging_practice/logging_lesson.py
@@ -3,11 +3,16 @@ import logging
 
 def loglevel_test():
     """
+    ログレベル：
     ログレベルを変更するには、basicConfigで設定可能。
     INFO : info以下表示、WARNING : warning以下表示といった具合。
     頭についてないと動作しなかった。既にログ吐いてからの設定は不可のよう。
+
+    ファイル出力：
+    filenameで指定可能。
+    ログは毎回追記されていた。
     """
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(filename="../logs/test.log", level=logging.DEBUG)
 
     """
     5つのログレベルが存在する。
@@ -19,7 +24,6 @@ def loglevel_test():
     logging.warning("想定外のことが発生、問題が起こりそう")
     logging.error("重大なエラーにより、機能の一部が停止")
     logging.critical("プログラム全体がダウン、重大なエラー")
-
 
 if __name__ == "__main__":
     loglevel_test()
